d6850a9e7b9dee1b70f3c67d5d6a41a073b64aff
aradhita21 <64492383+aradhita21@users.noreply.github.com>
2020-05-25 07:13:18 +0530

    Add files via upload

---
+++ config/oauth.js
@@ -0,0 +1,12 @@
+
+//app secret : 5d65535f4ecb09f523398aef9f8137c0
+//App ID: 2284950575134905
+
+var ids = {
+  facebook: {
+    clientID : '2284950575134905',
+    clientSecret : '5d65535f4ecb09f523398aef9f8137c0',
+    callbackURL: 'https://127.0.0.1:8443/auth/facebook/callback'
+  }
+}
+module.exports = ids;
